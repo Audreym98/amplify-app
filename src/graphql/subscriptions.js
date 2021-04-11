9fd922c0e040e360f834faba840f1e64ccb71f1b
audreym98 <audreym98@icloud.com>
2021-04-10 22:42:57 -0600

    Add GraphQL API and img storage

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      name
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      name
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      name
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
